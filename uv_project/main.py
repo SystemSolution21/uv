888f152ee890210881f28204c9e20d3d7a4a1253
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2025-02-24 16:59:15 +0900

    modified main.py test_main.py

--- uv_project/main.py
+++ uv_project/main.py
@@ -2,5 +2,9 @@ def main() -> None:
     print("Hello from uv-project!")
 
 
+def add_num(a: int, b: int) -> int:
+    return sum([a, b])
+
+
 if __name__ == "__main__":
     main()
--- uv_project/tests/test_main.py
+++ uv_project/tests/test_main.py
@@ -3,3 +3,7 @@ import requests
 
 def test_main() -> None:
     assert True
+
+
+def test_add_num(a: int = 2, b: int = 3) -> None:
+    assert sum([a, b]) == 5
