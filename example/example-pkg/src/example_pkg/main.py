7c06d8988eff71d3ca5f3f824ed62681ff2ddd79
SystemSolution21 <systemsolution21@gmail.com>
2026-02-21 18:32:59 +0900

    Update codebase.

---
+++ example/example-pkg/src/example_pkg/main.py
@@ -0,0 +1,10 @@
+def greetings(user: str = "World") -> None:
+    print(f"Hello {user}! Welcome to the Python package example example-pkg!")
+
+
+def main() -> None:
+    greetings()
+
+
+if __name__ == "__main__":
+    main()
